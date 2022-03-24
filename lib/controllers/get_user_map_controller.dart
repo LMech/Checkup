e33ac693c4f08da2b7c4ea9c835a1bc7e8393496
Michael George <lmech@github.com>
2022-03-24 13:21:46 +0200

    merging with updates branch 'not tested'

--- lib/controllers/get_user_map_controller.dart
+++ lib/controllers/get_user_map_controller.dart
@@ -1,38 +1,38 @@
-import 'package:checkup/helpers/helpers.dart';
-import 'package:checkup/models/models.dart';
-import 'package:checkup/views/auth/auth.dart';
-import 'package:checkup/views/components/components.dart';
-import 'package:checkup/views/tabbar_ui.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:async';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:get/get.dart';
+// import 'package:checkup/helpers/helpers.dart';
+// import 'package:checkup/models/models.dart';
+// import 'package:checkup/views/auth/auth.dart';
+// import 'package:checkup/views/components/components.dart';
+// import 'package:checkup/views/tabbar_ui.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'dart:async';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:get/get.dart';
 
-class GetUserMap extends GetxController {
+// class GetUserMap extends GetxController {
 
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-  bool isLoading = false;
+//   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+//   bool isLoading = false;
 
-  Map<String, dynamic>? userMap;
-  var  photoUrl ='';
-  var userId = "" ;
-  GetUserMap(){
+//   Map<String, dynamic>? userMap;
+//   var  photoUrl ='';
+//   var userId = "" ;
+//   GetUserMap(){
 
-  }
-  @override
-  void onReady() async {
-    //run every time auth state changes
+//   }
+//   @override
+//   void onReady() async {
+//     //run every time auth state changes
 
-    super.onReady();
-  }
+//     super.onReady();
+//   }
 
-  @override
-  void onClose() {
+//   @override
+//   void onClose() {
 
-    super.onClose();
-  }
+//     super.onClose();
+//   }
 
 
-}
+// }
