08616d187cf399d7ba522bbe28e1388ef1b5025c
Michael George <lmech@github.com>
2022-04-26 20:10:11 +0200

    Adding signout button to the profile page and some other tweaks

--- lib/views/tabbar_ui.dart
+++ lib/views/tabbar_ui.dart
@@ -16,7 +16,7 @@ class TabbarUI extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<AuthController>(
         init: AuthController(),
-        builder: (controller) => controller.firestoreUser.value!.uid == null
+        builder: (controller) => controller.firestoreUser.value?.uid == null
             ? const SafeArea(
                 child: Center(
                   child: CircularProgressIndicator(),
