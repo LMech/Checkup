ecfd0891a3097bf0b9ac2e08a5e74f96e5907f57
Michael George <lmech@github.com>
2022-04-28 21:22:53 +0200

    UI enhancements

--- lib/views/components/connection_card.dart
+++ lib/views/components/connection_card.dart
@@ -4,12 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ConnectionCard extends StatelessWidget {
-  final Map<String, dynamic>? connectionData;
   final ConnectionsController connectionsController = Get.find();
+
+  final Map<String, dynamic>? connectionData;
+  final bool isFriend;
 
   ConnectionCard({
     Key? key,
     required this.connectionData,
+    required this.isFriend,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -17,61 +20,90 @@ class ConnectionCard extends StatelessWidget {
       child: Container(
         height: 100,
         color: Colors.white,
-        child: Row(children: [
-          Expanded(
-            child: connectionData?['photoUrl'] == ''
-                ? Avatar(
-                    connectionData?['photoUrl'] ?? '',
-                    radius: 35,
-                    height: 150,
-                    width: 150,
-                  )
-                : CircleAvatar(
-                    child: ClipOval(
-                        child: FadeInImage.assetNetwork(
-                            placeholder: '',
-                            image: connectionData?['photoUrl'],
-                            fit: BoxFit.cover,
-                            width: 150,
-                            height: 150)),
-                    radius: 35),
-            flex: 2,
-          ),
-          Expanded(
-            child: Container(
-              alignment: Alignment.topLeft,
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 5,
-                    child: ListTile(
-                      title: Text(connectionData?['name'] ?? ''),
-                      subtitle: Text(connectionData?['email'] ?? ''),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 5,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              connectionsController.removeConnection(
-                                  connectionData?['email'] ?? 'e');
-                            },
-                            child: const Text(
-                              "Delete",
-                              style: TextStyle(color: Colors.red),
-                            ))
-                      ],
-                    ),
-                  )
-                ],
-              ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(children: [
+            Expanded(
+              child: connectionData?['photoUrl'] == ''
+                  ? Avatar(
+                      connectionData?['photoUrl'] ?? '',
+                      radius: 33,
+                      height: 150,
+                      width: 150,
+                    )
+                  : CircleAvatar(
+                      child: ClipOval(
+                          child: FadeInImage.assetNetwork(
+                              placeholder: '',
+                              image: connectionData?['photoUrl'],
+                              fit: BoxFit.cover,
+                              width: 150,
+                              height: 150)),
+                      radius: 33),
+              flex: 2,
             ),
-            flex: 8,
-          )
-        ]),
+            Expanded(
+              child: Container(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: ListTile(
+                        title: Text(connectionData?['name'] ?? ''),
+                        subtitle: Text(connectionData?['email'] ?? ''),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          isFriend
+                              ? TextButton(
+                                  onPressed: () {
+                                    connectionsController.removeConnection(
+                                        connectionData?['email'] ?? 'e');
+                                  },
+                                  child: const Text(
+                                    "Delete",
+                                    style: TextStyle(color: Colors.red),
+                                  ))
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    TextButton(
+                                        onPressed: () {
+                                          connectionsController.acceptRequest(
+                                              connectionData?['email'] ?? 'e');
+                                        },
+                                        child: const Text(
+                                          "Accept",
+                                          style: TextStyle(color: Colors.green),
+                                        )),
+                                    TextButton(
+                                        onPressed: () {
+                                          connectionsController
+                                              .removeConnection(
+                                                  connectionData?['email'] ??
+                                                      'e');
+                                        },
+                                        child: const Text(
+                                          "Reject",
+                                          style: TextStyle(color: Colors.red),
+                                        )),
+                                  ],
+                                )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              flex: 8,
+            )
+          ]),
+        ),
       ),
       elevation: 8,
       margin: const EdgeInsets.all(10),
